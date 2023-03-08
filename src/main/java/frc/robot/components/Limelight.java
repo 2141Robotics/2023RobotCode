7a9b85cb6f9c34a4b4dcf844fd5083d54c765ed7
2141Robotics <2141robotics@gmail.com>
2023-03-07 16:49:08 -0800

    start limelight
    Co-Authored-By: Mineinjava <65673396+Mineinjava@users.noreply.github.com>

--- src/main/java/frc/robot/components/Limelight.java
+++ src/main/java/frc/robot/components/Limelight.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
+import frc.robot.math.Vector3D;
 public class Limelight
 {    /*
      FIELD
@@ -15,17 +16,19 @@ public class Limelight
     
     
     */
-    Double[] offset1 = {17d, -24d, 46d};
-    Double[] offset2 = {17d, 0d, 46d};
-    Double[] offset3 = {17d, 24d, 46d};
-    Double[] offset4 = {8.3d, -24d, 17d};
-    Double[] offset5 = {8.3d, 0d, 2d};
-    Double[] offset6 = {8.3d, 24d, 2d};
-    Double[] offset7 = {-8d, -24d, -17.5d};
-    Double[] offset8 = {-8d, 0d, -17.5d};
-    Double[] offset9 = {-8d, 24d, -17.5d};
-    
+    Vector3D offset1 = new Vector3D (17d, -24d, 46d);
+    Vector3D offset2 = new Vector3D (17d, 0d, 46d);
+    Vector3D offset3 = new Vector3D (17d, 24d, 46d);
+    Vector3D offset4 = new Vector3D (8.3d, -24d, 17d);
+    Vector3D offset5 = new Vector3D (8.3d, 0d, 2d);
+    Vector3D offset6 = new Vector3D (8.3d, 24d, 2d);
+    Vector3D offset7 = new Vector3D (-8d, -24d, -17.5d);
+    Vector3D offset8 = new Vector3D (-8d, 0d, -17.5d);
+    Vector3D offset9 = new Vector3D (-8d, 24d, -17.5d);
+
     public Limelight() {
 
    }
+
+   
 }
