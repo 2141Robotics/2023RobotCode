25cb77bc5d51b431b48f018b0eff737f598c10ef
2141Robotics <2141robotics@gmail.com>
2023-03-21 17:01:40 -0700

    Update Arm.java
    Co-Authored-By: Mineinjava <65673396+Mineinjava@users.noreply.github.com>

--- src/main/java/frc/robot/components/Arm.java
+++ src/main/java/frc/robot/components/Arm.java
@@ -1,25 +1,31 @@
 package frc.robot.components;
 
-import javax.imageio.plugins.tiff.TIFFDirectory;
-
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.WPI_CANCoder;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import frc.robot.math.Vec2d;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-
+import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import frc.robot.math.Constants;
 
 public class Arm {
     WPI_TalonFX extender;
     WPI_TalonFX m1;
     WPI_TalonFX m2;
+    Claw claw = new Claw();
+    WPI_CANCoder canCoder;
+    Vec2d pos_GROUND = new Vec2d(55, -4);
+    Vec2d pos_MID = new Vec2d(55,28.5);
+    Vec2d pos_HIGH = new Vec2d(55,38.5);
+    Vec2d pos_COLLECT = new Vec2d(55,25.5);
+    Vec2d OFFSET = new Vec2d(-12, 15.5);
+    Vec2d VERTICAL = new Vec2d(0, 44);
+    Vec2d CURRENT_POS = new Vec2d(0,44);
 
     public void setupFalcon(WPI_TalonFX talon) {
         
@@ -42,17 +48,17 @@ public class Arm {
     talon.config_kI(PID_ID, Constants.PID_SETTINGS[2], Constants.MS_DELAY);
     talon.config_kD(PID_ID, Constants.PID_SETTINGS[3], Constants.MS_DELAY);
     System.out.println("done config");
-
     talon.configMotionAcceleration(512*9);
     talon.configMotionCruiseVelocity(15*9*2048/360);
     talon.configMotionSCurveStrength(5);
     talon.configNeutralDeadband(0.01);
     }
 
-    public Arm (int extenderID, int m1ID, int m2ID){
+    public Arm (int extenderID, int m1ID, int m2ID, int canCoderID){
         this.extender = new WPI_TalonFX(extenderID);
         this.m1 = new WPI_TalonFX(m1ID);
         this.m2 = new WPI_TalonFX(m2ID);
+        this.canCoder = new WPI_CANCoder(canCoderID);
     
 
     }
@@ -63,15 +69,36 @@ public class Arm {
         this.setupFalcon(m1);
         this.setupFalcon(m2);
         this.setupFalcon(extender);
+        this.m1.configRemoteFeedbackFilter(this.canCoder.getDeviceID(), RemoteSensorSource.CANCoder, 0);
+        this.m1.configSelectedFeedbackSensor(FeedbackDevice.RemoteSensor0);
+        this.canCoder.configFactoryDefault();
+        this.canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
+    
     }
 
     public void move(XboxController ctrlr) {
+        if (ctrlr.getYButton()) {
+            this.CURRENT_POS = this.pos_HIGH;
+        } else if (ctrlr.getXButton()) {
+            this.CURRENT_POS = this.pos_MID;
+        } else if (ctrlr.getBButton()) {
+            this.CURRENT_POS = this.pos_COLLECT;
+        } else if (ctrlr.getAButton()) {
+            this.CURRENT_POS = this.pos_GROUND;
+        }
 
+        if (ctrlr.getLeftBumper()) {
+            this.claw.open();
+        } else if (ctrlr.getRightBumper()) {
+            this.claw.close();
+        }
     }
     
     public void move(Vec2d vec) {
+
         this.setRotation(vec.getAngle());
         this.setLength(vec.getLength());
+
     }
 
     public void setRotation(Double angle){
