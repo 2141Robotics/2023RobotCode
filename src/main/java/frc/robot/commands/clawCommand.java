86bebcae34ef63c1627bbf3eb63d7180df679667
2141Robotics <2141robotics@gmail.com>
2023-03-21 17:01:34 -0700

    Create clawCommand.java
    Co-Authored-By: Mineinjava <65673396+Mineinjava@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/clawCommand.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.components.Arm;
+import frc.robot.components.Claw;
+import frc.robot.math.Vec2d;
+
+@Deprecated
+public class clawCommand extends CommandBase {
+    public Claw claw;
+    public Boolean open;
+
+   public clawCommand(Claw claw, Boolean open) {
+    this.claw = claw;
+    this.open = open;
+   }
+
+   @Override
+   public void execute(){
+    if (this.open) {
+        this.claw.open();
+    } else {
+        this.claw.close();
+    }
+   }
+
+   @Override
+   public boolean isFinished(){
+    this.execute();
+    return true;
+   }
+
+   @Override
+   public void end(boolean interupted){
+    System.out.println("arm moved");
+   }
+
+
+}
