d7f2c39b2d37381bdd8fadf07eaad079b13e0b45
2141Robotics <2141robotics@gmail.com>
2023-03-21 17:01:43 -0700

    Create Claw.java
    Co-Authored-By: Mineinjava <65673396+Mineinjava@users.noreply.github.com>

---
+++ src/main/java/frc/robot/components/Claw.java
@@ -0,0 +1,19 @@
+package frc.robot.components;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public class Claw {
+    DoubleSolenoid s1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    DoubleSolenoid s2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+    
+    public void open(){
+        s1.set(Value.kForward);
+        s2.set(Value.kForward);
+    }
+    public void close() {
+        s1.set(Value.kReverse);
+        s2.set(Value.kReverse);
+    }
+}
