1e94b08c8d81bca1d4a29ece9db494d08aeb432f
2141Robotics <2141robotics@gmail.com>
2023-03-23 17:43:14 -0700

    Update autoBalance.java
    Co-Authored-By: Mineinjava <65673396+Mineinjava@users.noreply.github.com>

--- src/main/java/frc/robot/commands/autoBalance.java
+++ src/main/java/frc/robot/commands/autoBalance.java
@@ -4,31 +4,60 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.components.Arm;
 import frc.robot.components.SwerveDrive;
+import frc.robot.math.Constants;
 import frc.robot.math.Vec2d;
 
-@Deprecated
 public class autoBalance extends CommandBase {
     AHRS gyro;
     SwerveDrive dt;
 
-   public autoBalance(AHRS gyro, SwerveDrive dt) {
-   this.gyro = gyro;
-   this.dt = dt;
+    //  Flat = 0
+    //  Clockewise = 1,2,3,4
+    //  Counter = 360, 359
+
+    public boolean movewithpitch(AHRS gyro, SwerveDrive dt){
+
+        if((this.gyro.getPitch() > 184) ){
+            this.dt.move(new Vec2d(0,Constants.TICKS_PER_INCH * 1), 0);
+            return false;
+        }
+
+        else if(this.gyro.getPitch() < 176){
+            this.dt.move(new Vec2d(0,Constants.TICKS_PER_INCH * -1), 0);
+            return false;
+        }
+
+
+        if((this.gyro.getPitch() > 356) || (this.gyro.getPitch() < 4)){
+            this.dt.stop();
+            return true;
+        }
+        return false;
+    }
+
+    public autoBalance(AHRS gyro, SwerveDrive dt) {
+        this.gyro = gyro;
+        this.dt = dt;
    }
 
-   @Override
-   public void execute(){
-    this.dt.move(new Vec2d(0,-0.1), 0);
-   }
+    @Override
+    public void execute(){
+        movewithpitch(this.gyro, this.dt);
+    }
 
-   @Override
-   public boolean isFinished(){
-    return (Math.abs(this.gyro.getPitch()) < 4);
-   }
+    @Override
+    public boolean isFinished(){
+        if((this.gyro.getPitch() > 356) || (this.gyro.getPitch() < 4)){
+            this.dt.stop();
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-   @Override
-   public void end(boolean interupted){
-    this.dt.stop();
+    @Override
+    public void end(boolean interupted){
+        this.dt.stop();
     System.out.println("arm moved");
    }
 
