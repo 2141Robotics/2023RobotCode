3d8d37db23d00fbce88f952bc5a8cabe0fe831c9
2141Robotics <2141robotics@gmail.com>
2023-03-23 17:43:26 -0700

    Create FieldMeasurments.java
    Co-Authored-By: Mineinjava <65673396+Mineinjava@users.noreply.github.com>

---
+++ src/main/java/frc/robot/math/FieldMeasurments.java
@@ -0,0 +1,26 @@
+package frc.robot.math;
+
+public class FieldMeasurments {
+    /*This is all in inches */
+
+    /*From the very back of the arena to the half court line - Kobe */
+    public static final int Half_Court = 268;
+
+    public static final int Left_Of_Charger_to_Left_Wall = 60;
+
+    public static final int Back_To_End_Of_Charger = 137;
+
+    public static final int Left_Of_Charger_to_Right_Barrier = 96;
+
+    public static final int Back_To_Middle_Of_Charger = 99;
+
+    public static final int Back_To_Start_Of_Charger = 61;
+
+    public static final int Right_Barrier = 76;
+
+    public static final int Right_Of_Charger_To_Right_Barrier = 62;
+
+    public static final int Grid_Distance_To_Cone = 19;
+
+    public static final int Grid_Distance_To_Cube = 42;
+}
